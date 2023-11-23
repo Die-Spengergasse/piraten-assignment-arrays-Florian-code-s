222f831111daf72bc31b8d29248444ce42d1a043
Florian-code-s <144543430+Florian-code-s@users.noreply.github.com>
2023-11-23 16:36:07 +0100

    Demo Änderung

--- src/main/java/at/spengergasse/Pirat.java
+++ src/main/java/at/spengergasse/Pirat.java
@@ -1,66 +1,118 @@
 package at.spengergasse;
+public class Pirat{
 
-// Änderung
-public class Pirat {
+    private String name;
+    private int gesundheit;
+    private boolean holzbein;
 
-    public Pirat(String name, int gesundheit, boolean holzbein) {
+    public Pirat(String name, int gesundheit, boolean holzbein){
+        setName(name);
+        setGesundheit(gesundheit);
+        setHolzbein(holzbein);
     }
 
-    public String getName() {
-        return name;
+    public void setName(String name){
+        if (name == null){
+            throw new IllegalArgumentException("Darf nicht NULL sein");
+        }
+        else if(name.equals("")){
+            // System.out.println("Arrgh! Keinen Namen fuer den alten Seebaeren uebergeben!");
+            throw new IllegalArgumentException("Arrgh! Keinen Namen fuer den alten Seebaeren uebergeben!");
+        }
+        else{
+            this.name = name;
+        }
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-    public void trinken (char getraenk) {
-
-    }
-
-    public String toString () {
-        return "";
-    }
-    public boolean isHolzbein() {
-        return holzbein;
+    public void setGesundheit(int gesundheit){
+        if (gesundheit >100 || gesundheit <0){
+            // System.out.println("Yo-ho-ho - Gültiger Bereich für die Gesundheit: 0 bis 100 Prozent");
+            throw new IllegalArgumentException("Yo-ho-ho - Gültiger Bereich für die Gesundheit: 0 bis 100 Prozent");
+        }
+        else{
+            this.gesundheit = gesundheit;
+        }
     }
 
-    public void setHolzbein(boolean holzbein) {
+    public void setHolzbein(boolean holzbein){
         this.holzbein = holzbein;
     }
 
-    private String name;
-    private int gesundheit;  // 0 - 100 
-    private boolean holzbein;
-    
-    
-    public Pirat() {
+    public String getName(){
+        return name;
     }
-    
-    
-    public void kielholen() {
-        
-        // Gesundheit darf nicht unter 0 kommen
-        // Pruefung hat er holzbein, dann 95 sonst 90
-        
-        // Einfache Variante mit Zwischenergebnis
-        int aktuelleGesundheit = gesundheit - 90;
-        
-        // Pruefen ob aktuelleGesundheit <= 0 -> dann gesundheit 0 und Meldung
-        int gesundheit = 0;
-        
-        // ...
-        
-    }
-    
-    public int getGesundheit() {
+
+    public int getGesundheit(){
         return gesundheit;
     }
-    
-    public void setGesundheit (int gesundheit) {
-        this.gesundheit = gesundheit;
+
+    public boolean getHolzbein(){
+        return holzbein;
     }
-    
-    
-    
-    
+
+    public void setTrinken(char getraenk){
+        if (getraenk == 'w'){
+            gesundheit -= 10;
+        }
+        if (getraenk == 'g'){
+            gesundheit += 10;
+        }
+        if (getraenk == 'r'){
+            gesundheit += 20;
+        }
+        if (gesundheit <=0){
+            gesundheit = 0;
+            if (gesundheit == 0){
+                System.out.println("Haudegen " + name + " ging ueber die Planken.");
+            }
+        }
+        if (gesundheit >=100){
+            gesundheit = 100;
+            if (gesundheit == 100){
+                System.out.println("Haudegen " + name + " erfreut sich voller Gesundheit");
+            }
+        }
+    }
+
+    public void kielholen(){
+        if(holzbein == true){
+            gesundheit -= 95;
+        }
+        else{
+            gesundheit -= 90;
+        }
+        if (gesundheit <=0){
+            gesundheit = 0;
+            if (gesundheit == 0){
+                System.out.println("Pirat " + name + " stieg in Davy Jone’s Kiste");
+            }
+        }
+    }
+
+    public String toString(){
+        String gesundheits_info;
+        String holzbein_info;
+        holzbein_info = "";
+        gesundheits_info = "";
+        if (gesundheit <= 0){
+            gesundheits_info = "X - sprang in die Kiste";
+        }
+        else if (gesundheit <=10){
+            gesundheits_info = gesundheit + "% - hisst den Yellow Jack";
+        }
+        else{
+            gesundheits_info = gesundheit + "%";
+        }
+        if (holzbein == true){
+            holzbein_info = "Holzbeiner";
+        }
+        else{
+            holzbein_info = "Zweifüßer";
+        }
+        return "'Aye' - Trunkenbold " + name + " meldet sich an Board! " + gesundheits_info + ", " + holzbein_info ;
+    }
+
+    public void print(){
+        System.out.println(toString());
+    }
 }
--- src/main/java/at/spengergasse/Schiff.java
+++ src/main/java/at/spengergasse/Schiff.java
@@ -1,52 +1,240 @@
 package at.spengergasse;
-
-public class Schiff{
-    
-    
+public class Schiff
+{
+    private int kanonen;
+    private char typ;
     private Pirat kapitaen;
+    private Pirat pirat1;
     private Pirat pirat2;
     private Pirat pirat3;
     private Pirat pirat4;
     private Pirat pirat5;
-    private Pirat pirat6;
-    
-    private char typ;
-    
-    public Schiff(char typ) {
-        setTyp(typ);
+
+    public char getTyp(){
+        return typ;
     }
-    
-    public void setTyp(char typ) {
-        this.typ = typ;
+
+    public int getKanonen(){
+        return kanonen;
     }
-    
-    
-    public void anheuern(Pirat pirat) {
-        
-        if(typ == 's') {
-            
-            // Beim typ s d�rfen nur 3 Piraten aufgenommen werden
-            if(this.kapitaen == null) {
-                this.kapitaen = pirat;
-            } else if(this.pirat2 == null) {
-                
-            } else if(this.pirat3 == null) {
-                
-            } else {
-                // Fehlermeldung
+
+    public Schiff(char typ){
+        setType(typ);
+        kanonenKlarmachen();
+    }
+
+    public void setType(char typ){
+        if (typ == 's'){
+            if(kapitaen != null && pirat1 != null && pirat2 != null){
+                // System.out.println("Harr! Der Schiffstyp " + typ + " ist zu raeudig!");
+                throw new IllegalArgumentException("Harr! Der Schiffstyp " + typ + " ist zu raeudig!");
             }
-            // ...
-            
-        } else if(typ == 'b') {
-            
-            if(this.kapitaen == null) {
-                // ...
+            else{
+                this.typ = typ;
+            }}
+        else if(typ == 'b'){
+            if(kapitaen != null && pirat1 != null && pirat2 != null && pirat3 != null){
+                // System.out.println("Harr! Der Schiffstyp " + typ + " ist zu raeudig!");
+                throw new IllegalArgumentException("Harr! Der Schiffstyp " + typ + " ist zu raeudig!");
             }
-            
+            else{
+                this.typ = typ;
+            }}
+        else if(typ == 'f'){
+            if(kapitaen != null && pirat1 != null && pirat2 != null && pirat3 != null && pirat4 != null){
+                // System.out.println("Harr! Der Schiffstyp " + typ + " ist zu raeudig!");
+                throw new IllegalArgumentException("Harr! Der Schiffstyp " + typ + " ist zu raeudig!");
+            }
+            else{
+                this.typ = typ;
+            }}
+        else if(typ == 'g'){
+            if(kapitaen != null && pirat1 != null && pirat2 != null && pirat3 != null && pirat4 != null && pirat5 != null){
+                // System.out.println("Harr! Der Schiffstyp " + typ + " ist zu raeudig!");
+                throw new IllegalArgumentException("Harr! Der Schiffstyp " + typ + " ist zu raeudig!");
+            }
+            else{
+                this.typ = typ;
+            }
         }
-        
+
     }
-    
-    
-    
+
+    public void kanonenKlarmachen(){
+        if(typ == 's'){
+            kanonen = 1;
+        }
+        else if(typ == 'b'){
+            kanonen = 2;
+        }
+        else if(typ == 'f'){
+            kanonen = 5;
+        }
+        else if(typ == 'g'){
+            kanonen = 6;
+        }
+        else{
+            throw new IllegalArgumentException("Arrgh! Schaebige Schnigge vom Typ " + typ + " ist nicht wuerdiggesegelt zu werden!");
+        }
+    }
+
+    public void anheuern(Pirat pirat){
+        int piratesMax;
+        piratesMax = 0;
+
+        if(typ == 's'){
+            piratesMax = 3;
+        }
+        if(typ == 'b'){
+            piratesMax = 4;
+        }
+        if(typ == 'f'){
+            piratesMax = 5;
+        }
+        if(typ == 'g'){
+            piratesMax = 6;
+        }
+
+        int pirates;
+        pirates = 0;
+
+        if(kapitaen != null)pirates++;
+        if(pirat1 != null) pirates++;
+        if(pirat2 != null) pirates++;
+        if(pirat3 != null) pirates++;
+        if(pirat4 != null) pirates++;
+        if(pirat5 != null) pirates++;
+
+        if(pirates >= piratesMax){
+            throw new IllegalArgumentException("AIII!! Noch mehr und das Schiff geht unter, Landratte.");
+        }
+
+        if(typ == 's'){
+            if(kapitaen == null){
+                kapitaen = pirat;
+            }
+            else if(pirat1 == null){
+                pirat1 = pirat;
+            }
+            else if(pirat2 == null){
+                pirat2 = pirat;
+            }
+        }
+        if(typ == 'b'){
+            if(kapitaen == null){
+                kapitaen = pirat;
+            }
+            else if(pirat1 == null){
+                pirat1 = pirat;
+            }
+            else if(pirat2 == null){
+                pirat2 = pirat;
+            }
+            else if(pirat3 == null){
+                pirat3 = pirat;
+            }
+        }
+        if(typ == 'f'){
+            if(kapitaen == null){
+                kapitaen = pirat;
+            }
+            else if(pirat1 == null){
+                pirat1 = pirat;
+            }
+            else if(pirat2 == null){
+                pirat2 = pirat;
+            }
+            else if(pirat3 == null){
+                pirat3 = pirat;
+            }
+            else if(pirat4 == null){
+                pirat4 = pirat;
+            }
+        }
+        if(typ == 'g'){
+            if(kapitaen == null){
+                kapitaen = pirat;
+            }
+            else if(pirat1 == null){
+                pirat1 = pirat;
+            }
+            else if(pirat2 == null){
+                pirat2 = pirat;
+            }
+            else if(pirat3 == null){
+                pirat3 = pirat;
+            }
+            else if(pirat4 == null){
+                pirat4 = pirat;
+            }
+            else if(pirat5 == null){
+                pirat5 = pirat;
+            }
+        }
+    }
+
+    public String toString(){
+        String typ_info;
+        String pirat_info_k;
+        String pirat_info_1;
+        String pirat_info_2;
+        String pirat_info_3;
+        String pirat_info_4;
+        String pirat_info_5;
+
+        int typ_groeße;
+        String komplett;
+
+        typ_groeße = 0;
+        typ_info = "";
+        pirat_info_k = "";
+        pirat_info_1 = "";
+        pirat_info_2 = "";
+        pirat_info_3 = "";
+        pirat_info_4 = "";
+        pirat_info_5 = "";
+
+        if(typ == 's'){
+            typ_info = "Schaluppe";
+            typ_groeße = 3;
+        }
+        else if (typ == 'b'){
+            typ_info = "Brigg";
+            typ_groeße = 4;
+        }
+        else if (typ == 'f'){
+            typ_info = "Fregatte";
+            typ_groeße = 5;
+        }
+        else if (typ == 'g'){
+            typ_info = "Galeone";
+            typ_groeße = 6;
+        }
+
+        if (kapitaen != null){
+            pirat_info_k = "Kapitän: " + kapitaen.toString() + "\n";
+        }
+        if (pirat1 != null){
+            pirat_info_1 = "Pirat 2: " + pirat1.toString() + "\n";
+        }
+        if (pirat2 != null){
+            pirat_info_2 = "Pirat 3: " + pirat2.toString() + "\n";
+        }
+        if (pirat3 != null){
+            pirat_info_3 = "Pirat 4: " + pirat3.toString() + "\n";
+        }
+        if (pirat4 != null){
+            pirat_info_4 = "Pirat 5: " + pirat4.toString() + "\n";
+        }
+        if (pirat5 != null){
+            pirat_info_5 = "Pirat 6: " + pirat5.toString() + "\n";
+        }
+        komplett = "******************************************\n" + "Crew " + typ_info + " (" +  typ_groeße + "):\n" + "-----------------------\n" + pirat_info_k + pirat_info_1 + pirat_info_2 + pirat_info_3 + pirat_info_4 + pirat_info_5 + "\n" + "******************************************";
+        return komplett;
+    }
+
+    public void print(){
+        System.out.println(toString());
+    }
 }
+
